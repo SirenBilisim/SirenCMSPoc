0481dbbbb3e5f71014ac59fad741e82f89d8bb24
mstfssmn <46896122+mstfssmn@users.noreply.github.com>
2019-01-24 14:27:49 +0200

    jwt token entegrasyonu yapıldı

--- SirencmsClient/src/app/_helpers/error.interceptor.ts
+++ SirencmsClient/src/app/_helpers/error.interceptor.ts
@@ -4,11 +4,13 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ToastrService } from "ngx-toastr";
 import { AuthenticationService } from '../_services/authentication.service';
+import {Router} from "@angular/router";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
     constructor(
         private authenticationService: AuthenticationService,
+        private router: Router,
         private toastr: ToastrService       // Toastr service for alert message
         ) { }
 
@@ -17,7 +19,13 @@ export class ErrorInterceptor implements HttpInterceptor {
             if (err.status === 401) {
                 // auto logout if 401 response returned from api
                 this.authenticationService.logout();
-                location.reload(true);
+                this.router.navigate(['login']);
+            }
+
+            if (err.status === 403) {
+                // auto logout if 401 response returned from api
+                this.toastr.error('Yetkisiz bir sayfaya erişim sağlamak istediniz.');
+                this.router.navigate(['home']);
             }
 
             if (err.status === 400) {
